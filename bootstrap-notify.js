f9bb99487435d246dcd0cdf2fe9a1be3f0dbfe80
aaron <aaron.bostick@dandsltd.com>
2018-05-10 06:42:36 +0000

    Default to global.

--- bootstrap-notify.js
+++ bootstrap-notify.js
@@ -9,16 +9,16 @@
 /* global define:false, require: false, jQuery:false */
 
 (function (factory) {
-	if (typeof define === 'function' && define.amd) {
-		// AMD. Register as an anonymous module.
-		define(['jquery'], factory);
-	} else if (typeof exports === 'object') {
-		// Node/CommonJS
-		factory(require('jquery'));
-	} else {
-		// Browser globals
-		factory(jQuery);
-	}
+	// if (typeof define === 'function' && define.amd) {
+	// 	// AMD. Register as an anonymous module.
+	// 	define(['jquery'], factory);
+	// } else if (typeof exports === 'object') {
+	// 	// Node/CommonJS
+	// 	factory(require('jquery'));
+	// } else {
+	// 	// Browser globals
+	factory(jQuery);
+	// }
 }(function ($) {
 	// Create the defaults once
 	var defaults = {
